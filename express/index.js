82a45941918b1e1116caf493d2d0119ddd464cc6
rushikesh kalhapure <rushikesh.kalhapure@druva.com>
2026-03-03 18:19:16 +0530

    express code

---
+++ express/index.js
@@ -0,0 +1,24 @@
+// const express = require('express');
+import express from 'express';
+import home from './pages/home.js';
+import about from './pages/about.js';
+
+const app = express();
+
+app.get('', (req, res) => {
+    res.send(home());
+});
+
+app.get('/about', (req, res) => {
+    res.send(about());
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+
+
+
+
+
---
+++ express/pages/home.js
@@ -0,0 +1,3 @@
+export default function home() {
+    return '<h1>Hello Home</h1>';
+}
